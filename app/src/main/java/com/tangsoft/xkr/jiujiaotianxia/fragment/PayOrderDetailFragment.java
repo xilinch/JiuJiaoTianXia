d762d478f19bde6f2f1051efe028fa9a86d0eb25
xilinch <308825747@qq.com>
2017-10-18 21:42:07 +0800

    支付

--- app/src/main/java/com/tangsoft/xkr/jiujiaotianxia/fragment/PayOrderDetailFragment.java
+++ app/src/main/java/com/tangsoft/xkr/jiujiaotianxia/fragment/PayOrderDetailFragment.java
@@ -30,7 +30,12 @@ import com.tangsoft.xkr.jiujiaotianxia.MainActivity;
 import com.tangsoft.xkr.jiujiaotianxia.R;
 import com.tangsoft.xkr.jiujiaotianxia.api.ApiConfig;
 import com.tangsoft.xkr.jiujiaotianxia.listener.DownloadResultDealWithListener;
+import com.tangsoft.xkr.jiujiaotianxia.wxapi.WXPayEntryActivity;
 import com.tangsoft.xkr.jiujiaotianxia.zfbapi.PayResult;
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelpay.PayReq;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -51,7 +56,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-
 /**
  * Created by Administrator on 2016-12-29.
  */
@@ -67,19 +71,19 @@ public class PayOrderDetailFragment extends DialogFragment {
 
     String zfbInfo = "";
 
+    String wxInfo = "";
 
-    ProgressDialog progressDialog ;
+    ProgressDialog progressDialog;
 
-     MainActivity activity = null;
+    MainActivity activity = null;
 
     public static final PayOrderDetailFragment newInstance(String orderId) {
         PayOrderDetailFragment fragment = new PayOrderDetailFragment();
         Bundle bundle = new Bundle();
-        bundle.putString("orderId",orderId);
+        bundle.putString("orderId", orderId);
         fragment.setArguments(bundle);
         return fragment;
     }
-
 
 
     private static final int SDK_PAY_FLAG = 1;
@@ -100,25 +104,27 @@ public class PayOrderDetailFragment extends DialogFragment {
                     // 判断resultStatus 为9000则代表支付成功
                     if (TextUtils.equals(resultStatus, "9000")) {
                         // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-                        if(activity!=null){
+                        if (activity != null) {
                             activity.loadPayResult();
                             Toast.makeText(activity, "支付成功", Toast.LENGTH_SHORT).show();
                         }
-                        Log.i("TAG","支付宝支付成功");
+                        Log.i("TAG", "支付宝支付成功");
 
 
                     } else {
                         // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-                        if(activity!=null) {
+                        if (activity != null) {
                             Toast.makeText(activity, "支付失败", Toast.LENGTH_SHORT).show();
                         }
 
-                        Log.i("TAG","支付宝支付失败");
+                        Log.i("TAG", "支付宝支付失败");
                     }
                     break;
                 }
             }
-        };
+        }
+
+        ;
     };
 
 
@@ -131,7 +137,7 @@ public class PayOrderDetailFragment extends DialogFragment {
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // 使用不带Theme的构造器, 获得的dialog边框距离屏幕仍有几毫米的缝隙。
-         dialog = new Dialog(getActivity(), R.style.BottomDialog);
+        dialog = new Dialog(getActivity(), R.style.BottomDialog);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 设置Content前设定
         dialog.setContentView(R.layout.fragment_pay_order_deatil);
         ButterKnife.bind(getActivity());
@@ -150,7 +156,7 @@ public class PayOrderDetailFragment extends DialogFragment {
         int height = size.y;
 
         lp.width = WindowManager.LayoutParams.MATCH_PARENT; // 宽度持平
-        lp.height = height*3/5;
+        lp.height = height * 3 / 5;
         window.setAttributes(lp);
         return dialog;
     }
@@ -159,9 +165,9 @@ public class PayOrderDetailFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO: inflate a fragment view
-        View rootView = inflater.inflate(R.layout.fragment_pay_order_deatil,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_pay_order_deatil, container, false);
         ButterKnife.bind(this, rootView);
-        orderId=getArguments().getString("orderId");
+        orderId = getArguments().getString("orderId");
         activity = (MainActivity) getActivity();
 
         progressDialog = new ProgressDialog(getActivity());
@@ -179,7 +185,7 @@ public class PayOrderDetailFragment extends DialogFragment {
     }
 
     @OnClick({R.id.image_cancel,
-            R.id.tv_pay_zfb, R.id.tv_pay_wx,R.id.tv_pay_unionpay})
+            R.id.tv_pay_zfb, R.id.tv_pay_wx, R.id.tv_pay_unionpay})
     public void onClick(View view) {
 
         Animation slide_left_to_left = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_left_to_left);
@@ -192,12 +198,14 @@ public class PayOrderDetailFragment extends DialogFragment {
                 break;
 
             case R.id.tv_pay_zfb:
-                        payZFB();
+                payZFB();
                 dialog.cancel();
                 break;
             case R.id.tv_pay_wx:            //微信支付
-                linPayWay.startAnimation(slide_left_to_right);
-                linPayWay.setVisibility(View.GONE);
+//                linPayWay.startAnimation(slide_left_to_right);
+//                linPayWay.setVisibility(View.GONE);
+                payWX();
+                dialog.cancel();
                 break;
             case R.id.tv_pay_unionpay:  //银联支付
                 linPayWay.startAnimation(slide_left_to_right);
@@ -206,17 +214,17 @@ public class PayOrderDetailFragment extends DialogFragment {
         }
     }
 
-    private void payZFB(){
+    private void payZFB() {
         try {
-           final URL url = new URL(ApiConfig.getHost()+"/WebChat/Api/AliPay/AliPayRequest.aspx");
-            Map<String,String> params = new HashMap<>();
-            params.put("orderno",orderId);
-           final JSONObject object = new JSONObject(params);
+            final URL url = new URL(ApiConfig.getHost() + "/WebChat/Api/AliPay/AliPayRequest.aspx");
+            Map<String, String> params = new HashMap<>();
+            params.put("orderno", orderId);
+            final JSONObject object = new JSONObject(params);
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     progressDialog.setMessage("正在获取支付订单数据,请稍后...");
-                    String data = postAliPay(url,object.toString(),"UTF-8");
+                    String data = postAliPay(url, object.toString(), "UTF-8");
                     toPayAliPay(data);
                 }
             }).start();
@@ -228,14 +236,14 @@ public class PayOrderDetailFragment extends DialogFragment {
 
     }
 
-    private void toPayAliPay(String data){
+    private void toPayAliPay(String data) {
 
         try {
             JSONObject jsonObject = new JSONObject(data);
-            Log.i("TAG","data = "+data);
-             zfbInfo = jsonObject.getString("alipay");
-            Log.i("TAG","zfbInfo = "+zfbInfo);
-            if(activity!=null&&zfbInfo.length()>0){
+            Log.i("TAG", "data = " + data);
+            zfbInfo = jsonObject.getString("alipay");
+            Log.i("TAG", "zfbInfo = " + zfbInfo);
+            if (activity != null && zfbInfo.length() > 0) {
                 Runnable payRunnable = new Runnable() {
                     @Override
                     public void run() {
@@ -252,15 +260,15 @@ public class PayOrderDetailFragment extends DialogFragment {
                 // 必须异步调用
                 Thread payThread = new Thread(payRunnable);
                 payThread.start();
-            }else {
-                Toast.makeText(activity,"调用支付宝异常!",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(activity, "调用支付宝异常!", Toast.LENGTH_SHORT).show();
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public  String postAliPay(URL url,String params, String encode) {
+    public String postAliPay(URL url, String params, String encode) {
 
         byte[] data = new byte[0];
         try {
@@ -270,8 +278,7 @@ public class PayOrderDetailFragment extends DialogFragment {
 
         }
         try {
-
-            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setConnectTimeout(10000);       //设置连接超时时间
             httpURLConnection.setDoInput(true);                  //打开输入流，以便从服务器获取数据
             httpURLConnection.setDoOutput(true);                 //打开输出流，以便向服务器提交数据
@@ -286,7 +293,7 @@ public class PayOrderDetailFragment extends DialogFragment {
             outputStream.write(data);
 
             int response = httpURLConnection.getResponseCode();            //获得服务器的响应码
-            if(response == HttpURLConnection.HTTP_OK) {
+            if (response == HttpURLConnection.HTTP_OK) {
                 InputStream inptStream = httpURLConnection.getInputStream();
                 return dealResponseResult(inptStream);                     //处理服务器的响应结果
             }
@@ -298,21 +305,89 @@ public class PayOrderDetailFragment extends DialogFragment {
         return "";
     }
 
-    public  String dealResponseResult(InputStream inputStream) {
-                String resultData = null;      //存储处理结果
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                byte[] data = new byte[1024];
-                 int len = 0;
-                 try {
-                         while((len = inputStream.read(data)) != -1) {
-                                byteArrayOutputStream.write(data, 0, len);
-                             }
-                     } catch (IOException e) {
-                         e.printStackTrace();
-                     }
-                resultData = new String(byteArrayOutputStream.toByteArray());
-                 return resultData;
-             }
+    public String dealResponseResult(InputStream inputStream) {
+        String resultData = null;      //存储处理结果
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int len = 0;
+        try {
+            while ((len = inputStream.read(data)) != -1) {
+                byteArrayOutputStream.write(data, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        resultData = new String(byteArrayOutputStream.toByteArray());
+        return resultData;
+    }
 
+    private void payWX() {
+        try {
+            final URL url = new URL(ApiConfig.getHost() + "/WebChat/Api/WxPay/WeiXinRequest.aspx");
+            Map<String, String> params = new HashMap<>();
+            params.put("orderno", orderId);
+            final JSONObject object = new JSONObject(params);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.setMessage("正在获取支付订单数据,请稍后...");
+                    final String data = postAliPay(url, object.toString(), "UTF-8");
+                    toPayWxPay(data);
+//                    if(getActivity() != null){
+//                        getActivity().runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                toPayWxPay(data);
+//                            }
+//                        });
+//                    }
+                }
+            }).start();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void toPayWxPay(String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            Log.i("TAG", "data = " + data);
+            //{"result":0,"message":"生成预支付交易单成功！","appid":"wx0b0180abce364a9d","partnerid":"1481491612",
+            // "prepayid":"wx201710182119147a4dda58b80996008510","package":"Sign=WXPay",
+            // "noncestr":"ca33ad5c416d4461a84884720b72fdb8","timestamp":"1508332755","sign":"3103569A268E318A74E797CB08436DEA"}
+            wxInfo = data;
+            if (activity != null && wxInfo.length() > 0) {
+                IWXAPI msgApi = WXAPIFactory.createWXAPI(activity, WXPayEntryActivity.WX_APPID);
+                try{
+                    if(!msgApi.isWXAppInstalled()){
+                        Toast.makeText(getContext(), "没有安装微信支付", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if(!msgApi.isWXAppSupportAPI()){
+                        Toast.makeText(getContext(), "微信不支持", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    PayReq request = new PayReq();
+                    request.appId = jsonObject.optString("appid");
+                    request.partnerId = jsonObject.optString("partnerid");
+                    request.prepayId= jsonObject.optString("partnerid");
+                    request.packageValue = jsonObject.optString("package");
+                    request.nonceStr= jsonObject.optString("noncestr");
+                    request.timeStamp= jsonObject.optString("timestamp");
+                    request.sign= jsonObject.optString("sign");
+                    msgApi.sendReq(request);
+                }catch (Exception e){
+                    Toast.makeText(getContext(), "没有安装微信支付", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();}
+            } else {
+                Toast.makeText(activity, "调用支付宝异常!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
